449d277ca94df91b69d540727a0307dbf705eeda
FikerTaddev <fekirtad.dev@hotmail.com>
2026-04-19 09:12:37 +0300

    feat: added user table schema

---
+++ ProjectManagement/backend/db/schema.sql
@@ -0,0 +1,21 @@
+-- Enable extensions (optional but good practice)
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+
+-- enum 
+
+CREATE TYPE ROLE AS ENUM('admin','member','viewer')
+
+-- USERS TABLE
+CREATE TABLE IF NOT EXISTS users (
+    id SERIAL PRIMARY KEY,
+    email TEXT NOT NULL UNIQUE,
+    password TEXT NOT NULL,
+    role TEXT NOT NULL DEFAULT 'user',
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+-- 
+
+-- -- Optional index for performance
+-- CREATE INDEX IF NOT EXISTS idx_users_email ON users(email);
